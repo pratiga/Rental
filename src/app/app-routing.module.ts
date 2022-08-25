3a62a29428075be7caf2447922571c3eed1ab0e4
pratiga <gaitrisanu2468@gmail.com>
2022-08-25 21:13:12 +0545

    path of routing
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OpinionComponent } from './opinion/opinion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'opinion', component:OpinionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
